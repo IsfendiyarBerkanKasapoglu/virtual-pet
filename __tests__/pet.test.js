ed71f5bfd5f9db6c9005001051ef2dfef139239f
Isfendiyar <berkankasapoglu03@hotmail.com>
2021-10-31 21:01:59 +0200

    last

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,6 +1,22 @@
 const Pet = require('../src/pet');
 describe('constructor', () => {
-    it('returns an object', () => {
+  it('returns an object', () => {
+      const pet = new Pet('Fido');
       expect(new Pet('Fido')).toBeInstanceOf(Object);
-    });
+   });
+});
+describe('constructor', () => {
+  it('has a initial age of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.age).toEqual(0);
   });
+});
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+  });
+});
